e5c40352fd534c503f1639c0123a9198a761d4e2
iman <iman_khan@firstfloor.agency>
2023-11-29 22:42:50 +0530

    first commit

--- components/test/login.js
+++
@@ -1,8 +0,0 @@
-import { React, useContext } from "react";
-import { LoginContext } from "@/pages/context/logincontext";
-const Login = () => {
-  const { name } = useContext(LoginContext);
-  return <div>{name}</div>;
-};
-
-export default Login;
